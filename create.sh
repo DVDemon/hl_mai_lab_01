380f11b75f83d3bc0c0adf1a728137281996815f
Malik Gazizullin <gazizullin.malik2010@yandex.ru>
2021-11-06 19:12:36 +0300

    hw1

---
+++ create.sh
@@ -0,0 +1,14 @@
+sudo mysql -e '
+create database if not exists stud;
+use stud;
+create user if not exists "stud" identified by "stud";
+grant all on stud.* to "stud";
+CREATE TABLE IF NOT EXISTS `Person`
+(
+    `login`  VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `first_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `last_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+    `age` INTEGER NOT NULL,PRIMARY KEY (`login`),
+    INDEX `login` (`login`)
+);
+'
