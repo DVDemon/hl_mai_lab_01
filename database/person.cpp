becc0136c54c1dc0bb64c5d7078347b79f770915
Malik Gazizullin <gazizullin.malik2010@yandex.ru>
2021-11-06 19:07:07 +0300

    hw1

--- database/person.cpp
+++ database/person.cpp
@@ -1,4 +1,4 @@
-#include "author.h"
+#include "person.h"
 #include "database.h"
 #include "../config/config.h"
 
@@ -19,7 +19,20 @@ using Poco::Data::Statement;
 namespace database
 {
 
-    void Author::init()
+    void generate_data() {
+        for(auto& login_prefix : {"start_prefix_", "___", "official_", ""}) {
+            for(std::string first_name : {"kaizer", "mnemonic", "ruler"}) {
+                for(auto& last_name : {"kaizer", "mnemonic", "ruler"}) {
+                    for(auto age: {10, 20, 30, 40}) {
+                        Person(login_prefix + first_name + last_name + std::to_string(age), first_name,
+                               last_name, age).save_to_mysql();
+                    }
+                }
+            }
+        }
+    }
+
+    void Person::init()
     {
         try
         {
@@ -27,18 +40,20 @@ namespace database
             Poco::Data::Session session = database::Database::get().create_session();
             //*
             Statement drop_stmt(session);
-            drop_stmt << "DROP TABLE IF EXISTS Author", now;
+            drop_stmt << "DROP TABLE IF EXISTS Person", now;
             //*/
 
             // (re)create table
             Statement create_stmt(session);
-            create_stmt << "CREATE TABLE IF NOT EXISTS `Author` (`id` INT NOT NULL AUTO_INCREMENT,"
+
+            create_stmt << "CREATE TABLE IF NOT EXISTS `Person` (`login`  VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,"
                         << "`first_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,"
                         << "`last_name` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,"
-                        << "`email` VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,"
-                        << "`title` VARCHAR(1024) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL,"
-                        << "PRIMARY KEY (`id`),KEY `fn` (`first_name`),KEY `ln` (`last_name`));",
+                        << "`age` INTEGER NOT NULL,"
+                        << "PRIMARY KEY (`login`),"
+                        << "INDEX `login` (`login`));",
                 now;
+            generate_data();
         }
 
         catch (Poco::Data::MySQL::ConnectionException &e)
@@ -54,49 +69,46 @@ namespace database
         }
     }
 
-    Poco::JSON::Object::Ptr Author::toJSON() const
+    Poco::JSON::Object::Ptr Person::toJSON() const
     {
         Poco::JSON::Object::Ptr root = new Poco::JSON::Object();
 
-        root->set("id", _id);
+        root->set("login", _login);
         root->set("first_name", _first_name);
         root->set("last_name", _last_name);
-        root->set("email", _email);
-        root->set("title", _title);
+        root->set("age", _age);
 
         return root;
     }
 
-    Author Author::fromJSON(const std::string &str)
+    Person Person::fromJSON(const std::string &str)
     {
-        Author author;
+        Person Person;
         Poco::JSON::Parser parser;
         Poco::Dynamic::Var result = parser.parse(str);
         Poco::JSON::Object::Ptr object = result.extract<Poco::JSON::Object::Ptr>();
 
-        author.id() = object->getValue<long>("id");
-        author.first_name() = object->getValue<std::string>("first_name");
-        author.last_name() = object->getValue<std::string>("last_name");
-        author.email() = object->getValue<std::string>("email");
-        author.title() = object->getValue<std::string>("title");
+        Person.login() = object->getValue<std::string>("login");
+        Person.first_name() = object->getValue<std::string>("first_name");
+        Person.last_name() = object->getValue<std::string>("last_name");
+        Person.age() = object->getValue<int>("age");
 
-        return author;
+        return Person;
     }
 
-    Author Author::read_by_id(long id)
+    Person Person::read_by_login(std::string login)
     {
         try
         {
             Poco::Data::Session session = database::Database::get().create_session();
             Poco::Data::Statement select(session);
-            Author a;
-            select << "SELECT id, first_name, last_name, email, title FROM Author where id=?",
-                into(a._id),
+            Person a;
+            select << "SELECT login, first_name, last_name, age FROM Person where login=?",
+                into(a._login),
                 into(a._first_name),
                 into(a._last_name),
-                into(a._email),
-                into(a._title),
-                use(id),
+                into(a._age),
+                use(login),
                 range(0, 1); //  iterate over result set one row at a time
             select.execute();
             Poco::Data::RecordSet rs(select);
@@ -118,20 +130,19 @@ namespace database
         }
     }
 
-    std::vector<Author> Author::read_all()
+    std::vector<Person> Person::read_all()
     {
         try
         {
             Poco::Data::Session session = database::Database::get().create_session();
             Statement select(session);
-            std::vector<Author> result;
-            Author a;
-            select << "SELECT id, first_name, last_name, email, title FROM Author",
-                into(a._id),
+            std::vector<Person> result;
+            Person a;
+            select << "SELECT login, first_name, last_name, age FROM Person",
+                into(a._login),
                 into(a._first_name),
                 into(a._last_name),
-                into(a._email),
-                into(a._title),
+                into(a._age),
                 range(0, 1); //  iterate over result set one row at a time
 
             while (!select.done())
@@ -155,25 +166,34 @@ namespace database
         }
     }
 
-    std::vector<Author> Author::search(std::string first_name, std::string last_name)
+    void addPercentsToName(std::string& name) {
+        if (name.length() == 0) {
+            name = "%";
+        } else {
+            name = "%" + name + "%";
+        }
+    }
+
+    std::vector<Person> Person::search(std::string first_name, std::string last_name)
     {
         try
         {
             Poco::Data::Session session = database::Database::get().create_session();
             Statement select(session);
-            std::vector<Author> result;
-            Author a;
-            first_name+="%";
-            last_name+="%";
-            select << "SELECT id, first_name, last_name, email, title FROM Author where first_name LIKE ? and last_name LIKE ?",
-                into(a._id),
+            std::vector<Person> result;
+            Person a;
+
+            addPercentsToName(last_name);
+            addPercentsToName(first_name);
+
+            select << "SELECT login, first_name, last_name, age FROM Person where first_name LIKE ? and last_name LIKE ?",
+                into(a._login),
                 into(a._first_name),
                 into(a._last_name),
-                into(a._email),
-                into(a._title),
+                into(a._age),
                 use(first_name),
                 use(last_name),
-                range(0, 1); //  iterate over result set one row at a time
+                range(1, 1); //  iterate over result set one row at a time
 
             while (!select.done())
             {
@@ -197,7 +217,7 @@ namespace database
     }
 
    
-    void Author::save_to_mysql()
+    void Person::save_to_mysql()
     {
 
         try
@@ -205,24 +225,13 @@ namespace database
             Poco::Data::Session session = database::Database::get().create_session();
             Poco::Data::Statement insert(session);
 
-            insert << "INSERT INTO Author (first_name,last_name,email,title) VALUES(?, ?, ?, ?)",
+            insert << "INSERT INTO Person (login, first_name,last_name,age) VALUES(?, ?, ?, ?)",
+                use(_login),
                 use(_first_name),
                 use(_last_name),
-                use(_email),
-                use(_title);
+                use(_age);
 
             insert.execute();
-
-            Poco::Data::Statement select(session);
-            select << "SELECT LAST_INSERT_ID()",
-                into(_id),
-                range(0, 1); //  iterate over result set one row at a time
-
-            if (!select.done())
-            {
-                select.execute();
-            }
-            std::cout << "inserted:" << _id << std::endl;
         }
         catch (Poco::Data::MySQL::ConnectionException &e)
         {
@@ -237,53 +246,43 @@ namespace database
         }
     }
 
-    long Author::get_id() const
+    const std::string& Person::get_login() const
     {
-        return _id;
+        return _login;
     }
 
-    const std::string &Author::get_first_name() const
+    const std::string &Person::get_first_name() const
     {
         return _first_name;
     }
 
-    const std::string &Author::get_last_name() const
+    const std::string &Person::get_last_name() const
     {
         return _last_name;
     }
 
-    const std::string &Author::get_email() const
+    int Person::get_age() const
     {
-        return _email;
+        return _age;
     }
 
-    const std::string &Author::get_title() const
+    std::string &Person::login()
     {
-        return _title;
+        return _login;
     }
 
-    long &Author::id()
-    {
-        return _id;
-    }
-
-    std::string &Author::first_name()
+    std::string &Person::first_name()
     {
         return _first_name;
     }
 
-    std::string &Author::last_name()
+    std::string &Person::last_name()
     {
         return _last_name;
     }
 
-    std::string &Author::email()
+    int &Person::age()
     {
-        return _email;
-    }
-
-    std::string &Author::title()
-    {
-        return _title;
+        return _age;
     }
 }
---
+++ web_server/handlers/person_handler.h
@@ -0,0 +1,150 @@
+#ifndef PERSONHANDLER_H
+#define PERSONHANDLER_H
+
+#include "Poco/Net/HTTPServer.h"
+#include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Net/HTTPRequestHandlerFactory.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTTPServerRequest.h"
+#include "Poco/Net/HTTPServerResponse.h"
+#include "Poco/Net/HTTPServerParams.h"
+#include "Poco/Net/HTMLForm.h"
+#include "Poco/Net/ServerSocket.h"
+#include "Poco/Timestamp.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/DateTimeFormat.h"
+#include "Poco/Exception.h"
+#include "Poco/ThreadPool.h"
+#include "Poco/Util/ServerApplication.h"
+#include "Poco/Util/Option.h"
+#include "Poco/Util/OptionSet.h"
+#include "Poco/Util/HelpFormatter.h"
+
+#include "Poco/JSON/Parser.h"
+#include "Poco/JSON/Object.h"
+
+#include <iostream>
+#include <fstream>
+
+using Poco::DateTimeFormat;
+using Poco::DateTimeFormatter;
+using Poco::ThreadPool;
+using Poco::Timestamp;
+using Poco::Net::HTMLForm;
+using Poco::Net::HTTPRequestHandler;
+using Poco::Net::HTTPRequestHandlerFactory;
+using Poco::Net::HTTPServer;
+using Poco::Net::HTTPServerParams;
+using Poco::Net::HTTPServerRequest;
+using Poco::Net::HTTPServerResponse;
+using Poco::Net::NameValueCollection;
+using Poco::Net::ServerSocket;
+using Poco::Util::Application;
+using Poco::Util::HelpFormatter;
+using Poco::Util::Option;
+using Poco::Util::OptionCallback;
+using Poco::Util::OptionSet;
+using Poco::Util::ServerApplication;
+
+#include "../../database/person.h"
+
+class PersonHandler : public HTTPRequestHandler
+{
+public:
+    PersonHandler(const std::string &format) : _format(format)
+    {
+    }
+
+    void getByLogin(const HTMLForm& form, std::ostream &ostr) {
+        const std::string login = form.get("login");
+        database::Person result = database::Person::read_by_login(login);
+        Poco::JSON::Stringifier::stringify(result.toJSON(), ostr);
+    }
+
+    void getByName(const HTMLForm& form, std::ostream &ostr) {
+        std::string  fn = form.get("first_name", "");
+        std::string  ln = form.get("last_name", "");
+        auto results = database::Person::search(fn,ln);
+        if (results.size() == 0) {
+            ostr << "{ \"result\": false , \"reason\": \"not found\" }";
+            return;
+        }
+        Poco::JSON::Array arr;
+        for (auto s : results)
+            arr.add(s.toJSON());
+        Poco::JSON::Stringifier::stringify(arr, ostr);
+    }
+
+    void createPerson(HTTPServerRequest &request, std::ostream &ostr) {
+        try
+        {
+            Poco::JSON::Parser parser;
+            Poco::JSON::Object::Ptr json = parser.parse(request.stream()).extract<Poco::JSON::Object::Ptr>();
+
+            if (json->has("login") && json->has("age") && json->has("last_name") &&
+                json->has("first_name"))
+            {
+                database::Person person(
+                        json->getValue<std::string>("login"),
+                        json->getValue<std::string>("first_name"),
+                        json->getValue<std::string>("last_name"),
+                        json->getValue<int>("age")
+                                );
+
+                person.save_to_mysql();
+                ostr << "{ \"result\": true }";
+
+            } else {
+                ostr << "{\"result\": \"Not all fields exist: [login, age, last_name, first_name]\"}";
+            }
+        } catch(Poco::JSON::JSONException& jsone)
+        {
+            ostr << "{\"result\": \"Error while parsing. "<< jsone.message() << "\"}";
+        }
+    }
+
+    void handleRequest(HTTPServerRequest &request,
+                       HTTPServerResponse &response)
+    {
+        response.setChunkedTransferEncoding(true);
+        response.setContentType("application/json");
+        std::ostream &ostr = response.send();
+
+        if (request.getMethod() == "GET") { // Searching smth
+            try {
+                HTMLForm form(request, request.stream());
+                if (form.has("login")) {
+                    getByLogin(form, ostr);
+                    return;
+                } else if ((form.has("first_name")) || (form.has("last_name"))) {
+                    getByName(form, ostr);
+                    return;
+                }
+            }  catch (...)
+            {
+                ostr << "{ \"result\": false , \"reason\": \"not found\" }";
+                return;
+            }
+
+            auto results = database::Person::read_all();
+            Poco::JSON::Array arr;
+            for (auto s : results)
+                arr.add(s.toJSON());
+            Poco::JSON::Stringifier::stringify(arr, ostr);
+        } else { // adding new person
+            try
+            {
+                createPerson(request, ostr);
+            }
+            catch (...)
+            {
+                ostr << "{ \"result\": false , \"reason\": \" database error\" }";
+                return;
+            }
+        }
+    }
+
+private:
+    std::string _format;
+};
+#endif // !PERSONHANDLER_H
